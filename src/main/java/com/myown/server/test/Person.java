e21248adc84e95b989f29f37113b5650a7db4872
RASHAKOJAN <rashakoujan@outlook.com>
2022-04-03 00:25:59 +0300

    Constructor Injection Dependent List.

--- src/main/java/com/myown/server/test/Person.java
+++ src/main/java/com/myown/server/test/Person.java
@@ -1,10 +1,14 @@
 package com.myown.server.test;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Person {
 
     private int id;
     private String name;
     private Address address;
+    private List<String> addresses;
 
 
     public Person() {
@@ -21,8 +25,19 @@ public class Person {
         this.address = address;
     }
 
+    public Person(int id, String name, Address address, List<String> addresses) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.addresses = addresses;
+    }
+
     public void display(){
         System.out.println(" id:" + id +" name:" + name);
         System.out.println(" adress:" + address.infoAddress());
+        Iterator<String> iterator = addresses.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 }
