df20eeba1ee5c96ea277f277b6abf833340a76a1
RASHAKOJAN <rashakoujan@outlook.com>
2022-04-03 00:36:37 +0300

    Constructor Injection Dependent Map.

--- src/main/java/com/myown/server/test/Person.java
+++ src/main/java/com/myown/server/test/Person.java
@@ -2,6 +2,8 @@ package com.myown.server.test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Person {
 
@@ -9,6 +11,7 @@ public class Person {
     private String name;
     private Address address;
     private List<String> addresses;
+    private Map<String, String> description;
 
 
     public Person() {
@@ -32,12 +35,27 @@ public class Person {
         this.addresses = addresses;
     }
 
+    public Person(int id, String name, Address address, List<String> addresses, Map<String, String> description) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.addresses = addresses;
+        this.description = description;
+    }
+
     public void display(){
         System.out.println(" id:" + id +" name:" + name);
         System.out.println(" adress:" + address.infoAddress());
         Iterator<String> iterator = addresses.iterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
+        }
+
+        Set<Map.Entry<String, String>> entrySet = description.entrySet();
+        Iterator<Map.Entry<String, String>> it = entrySet.iterator();
+        while (it.hasNext()){
+            Map.Entry<String, String> entry = it.next();
+            System.out.println(" key: " + entry.getKey() + " value: " + entry.getValue());
         }
     }
 }
